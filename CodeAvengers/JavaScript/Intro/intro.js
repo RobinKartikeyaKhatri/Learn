fc20c61ea42e5c557c65e406599745a25559e40f
pachelbel117 <pachelbel117@gmail.com>
2015-09-23 19:20:21 -0700

    Code Avengers

--- CodeAvengers/JavaScript/Intro/intro.js
+++ CodeAvengers/JavaScript/Intro/intro.js
@@ -3,3 +3,26 @@ alert('Your age is ' + age)
 
 var color = prompt('What is your favorite color?')
 alert(color + ' is a nice color')
+
+
+
+var guess = prompt('2 x 2 x 2 x 2 x 2 x 2 x 2 x 2 ?')
+
+if (guess == '256')
+  alert('Correct!')
+else
+  alert('Incorrect!')
+
+
+
+var score = 0
+
+var guess = prompt('What is phishing?\n1. Computer virus\n2. Corrupt website\n3. Junk email\n4. Identity theft scheme')
+if (guess == '4')
+  score = score + 1
+
+guess = prompt('What is a firewall?\n1. Antivirus program\n2. Internet blocker\n3. Internet filter\n4. Internet logger')
+if (guess == '3')
+  score = score + 1
+
+alert('You scored ' + score)
